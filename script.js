5a205d0744a7967145369a0f319f8216f1dd4911
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-04-13 16:42:02 +0800

    polished animations, button depth, navbar events

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const navbar = document.getElementById('nav');
-const hero = document.getElementById('main-nav');
+const bigNav = document.getElementById('main-nav');
 const mobileButton = document.getElementById('menu-toggle');
+const heading = document.getElementById('header');
+const returnButton = document.getElementById('returnButton');
+let hideTimeout;
+
+window.addEventListener('scroll', () => {
+  const mainHeading = heading.getBoundingClientRect();
+  if (mainHeading.bottom <= 0) {
+    returnButton.classList.remove('hidden')
+  }
+  else {
+    returnButton.classList.add('hidden')
+  }
+})
 
 mobileButton.addEventListener('click', () => {
   navbar.classList.toggle('hidden');
 });
 
 window.addEventListener('scroll', () => {
-  const mainNav = hero.getBoundingClientRect();
+  const mainNav = bigNav.getBoundingClientRect();
   if ((mainNav.bottom <= 0 || mainNav.top >= 30) && window.innerWidth >= 768) { 
     navbar.classList.remove('hidden');
   } else {
     navbar.classList.add('hidden');
   }
 });
-
 
 window.addEventListener('resize', () => {
   if (window.innerWidth <= 768) {
