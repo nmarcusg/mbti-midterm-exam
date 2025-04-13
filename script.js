34bd1618f439ef3000a9a1bff0b0faf315f428f1
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-04-13 14:32:23 +0800

    fixed nav for mobile

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const navbar = document.querySelector('.nav');
+const navbar = document.getElementById('nav');
 const hero = document.getElementById('main-nav');
+const mobileButton = document.getElementById('menu-toggle');
+
+mobileButton.addEventListener('click', () => {
+  navbar.classList.toggle('hidden');
+});
 
 window.addEventListener('scroll', () => {
-  const heroRect = hero.getBoundingClientRect();
-  if (heroRect.bottom <= 0 || heroRect.top >= 30) { 
+  const mainNav = hero.getBoundingClientRect();
+  if ((mainNav.bottom <= 0 || mainNav.top >= 30) && window.innerWidth >= 768) { 
     navbar.classList.remove('hidden');
   } else {
     navbar.classList.add('hidden');
   }
 });
+
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth <= 768) {
+    navbar.classList.add('hidden');
+  } else {
+    navbar.classList.remove('hidden');
+  }
+})
+
